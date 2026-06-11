0465e6a92eb9028fbd37437d75590efa2f5a2459
nguedja <divan.nguedja@facsciences-uy1.cm>
2026-06-11 11:51:36 +0200

    Fix Render deployment Flask

--- app.py
+++ app.py
@@ -187,7 +187,8 @@ def toutes_parties():
 # LANCEMENT
 # ==================================================
 
+import os
+
 if __name__ == "__main__":
-    app.run(
-        debug=True
-    )
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
